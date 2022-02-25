a52b4cb09cf625185e3308dc71eef7c1db8628e4
fatmaBe <73782362+fatmaBe@users.noreply.github.com>
2022-02-25 22:59:40 +0100

    fatma

--- MissionsBackEnd/MissionBackEnd/src/test/java/missionbackend/MissionBackEndApplicationTests.java
+++ MissionsBackEnd/MissionBackEnd/src/test/java/missionbackend/MissionBackEndApplicationTests.java
@@ -1,10 +1,10 @@
-package tn.esprit.missionsbackend;
+package missionbackend;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
-class MissionsBackEndApplicationTests {
+class MissionBackEndApplicationTests {
 
     @Test
     void contextLoads() {
