d9e5a4f89570eb4accfe54a2bd5c8f1e78cdf569
Wiemifaoui <73914179+wiemifaoui@users.noreply.github.com>
2022-03-23 03:11:33 +0100

    Update InvitationRepository.java

--- BusnissAndTravel LES 1er partie crud and user/src/main/java/tn/esprit/spring/Repository/InvitationRepository.java
+++ BusnissAndTravel LES 1er partie crud and user/src/main/java/tn/esprit/spring/Repository/InvitationRepository.java
@@ -1,11 +1,9 @@
 package tn.esprit.spring.Repository;
 
 import org.springframework.data.repository.CrudRepository;
-import org.springframework.stereotype.Repository;
 
 import tn.esprit.spring.entities.Invitation;
-@Repository
-public interface InvitationRepository extends CrudRepository<Invitation, Long>{
 
-	
+public interface InvitationRepository extends CrudRepository<Invitation,Long> {
+
 }
